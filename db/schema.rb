054af3ccf0e38117dcdb729ceb4f82f7b1575204
Yang Liu <jeremyrobturtle@gmail.com>
2018-05-03 00:20:53 -0700

    use pg for database

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20180503055048) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
-    t.integer "user_id"
-    t.text "body"
+    t.bigint "user_id", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
